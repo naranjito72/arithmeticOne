752a3a2d1616a098739a0380269585f5dc9eb6ea
naranjito72 <theclick36@gmail.com>
2021-08-23 17:34:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"#Aritmetic Intervals" 
---
+++ functions.js
@@ -0,0 +1,17 @@
+*use strict*
+  let myInter = [[-5,-4],[-3,-2],[1,2],[3,5],[8,9]];
+  let myRem = [-1,4];
+
+  function myIntervals(intervals, toBeRemoved) {
+    let [left,right] = toBeRemoved
+    let resArr = []
+
+    for(let [l,r] of intervals){
+      (r <= left || l >= right) ? resArr.push([l,r]) :
+      (l < left) ? resArr.push([l, left]) :
+      (r > right) ?  resArr.push([right, r]) : resArr;
+    }
+
+    return resArr;
+  };
+const showResult = () => console.log(myIntervals(myInter, myRem));
